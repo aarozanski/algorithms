09402cd1245b7d744ccf9b2a8e23afa2783085ce
aidarozanski <zhu.a.pdrsn@gmail.com>
2025-04-24 22:13:09 -0500

    Two Sums

---
+++ Week_16/src/test/java/TwoSumss.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class TwoSumss {
+
+
+    public static void main(String[] args) {
+        TwoSumss sums = new TwoSumss();
+        int [] result = sums.twoSums(new int[] {3,5,7}, 10);
+        System.out.println("Indices: " + result[0] + " : " + result[1]);
+
+    }
+
+    public int [] twoSums(int [] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for ( int i = 0; i < nums.length; i ++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)){
+                return new int [] {map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[0];
+    }
+
+}
