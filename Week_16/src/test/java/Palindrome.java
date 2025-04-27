782d819c991c34bebd39768d8c6041e58287ad70
aidarozanski <zhu.a.pdrsn@gmail.com>
2025-04-26 22:27:09 -0500

    Two Sums

---
+++ Week_16/src/test/java/Palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome {
+    public boolean isPalindrome(int x) {
+        // Step 1: Negative numbers are NEVER palindrome
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x; // Save the original number
+        int reversed = 0; // We will build the reversed number here
+
+        while (x != 0) {
+            int lastDigit = x % 10;          // Get the last digit
+            reversed = reversed * 10 + lastDigit; // Build the reversed number
+            x = x / 10;                      // Remove the last digit
+        }
+
+        return original == reversed; // Check if the original is the same as reversed
+    }
+}
+
